0539432561e4dcb6be445d16102e20a4546d02db
bagriel-d <dammerbr@gmail.com>
2025-11-04 00:16:58 +0000

    mend

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){ 
-    return <h1> Oi ,meu amor!!!
+    return <h1> Oi!!!
         Eu tô aprendendo a fazer um site, e quero usar a Suit pra montar meus projetos do curso. Pode me enviar as artes da Suit? :)</h1>
 }
 
