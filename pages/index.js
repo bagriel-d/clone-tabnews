738c016801e2d9744615bab1c007a4266e8c5e19
bagriel-d <dammerbr@gmail.com>
2025-11-04 00:11:24 +0000

    Aadicionando a pages

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home(){ 
+    return <h1> Oi ,meu amor!!!
+        Eu tô aprendendo a fazer um site, e quero usar a Suit pra montar meus projetos do curso. Pode me enviar as artes da Suit? :)</h1>
+}
+
+
+export default Home;
